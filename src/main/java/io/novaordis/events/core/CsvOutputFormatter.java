80feec6262985d1c5b21deadd8c1771b422ac1c3
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-08-17 18:27:09 -0700

    release 2.4.2-SNAPSHOT-11

--- src/main/java/io/novaordis/events/core/CsvOutputFormatter.java
+++ src/main/java/io/novaordis/events/core/CsvOutputFormatter.java
@@ -25,6 +25,8 @@ import io.novaordis.events.core.event.Property;
 import io.novaordis.events.core.event.ShutdownEvent;
 import io.novaordis.events.core.event.TimedEvent;
 import io.novaordis.utilities.time.Timestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -52,6 +54,9 @@ import java.util.StringTokenizer;
 public class CsvOutputFormatter implements OutputStreamConversionLogic {
 
     // Constants -------------------------------------------------------------------------------------------------------
+
+    private static final Logger log = LoggerFactory.getLogger(CsvOutputFormatter.class);
+    private static final boolean debug = log.isDebugEnabled();
 
     // MM/dd/yy HH:mm:ss (07/25/16 14:00:00) is the default time format so it works straight away with Excel
 
@@ -89,6 +94,8 @@ public class CsvOutputFormatter implements OutputStreamConversionLogic {
 
     @Override
     public boolean process(Event inputEvent) throws ClosedException {
+
+        if (debug) { log.debug(this + " processing " + inputEvent); }
 
         if (closed) {
             throw new ClosedException(this + " closed");
