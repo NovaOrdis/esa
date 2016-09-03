4f8ef6d4bc00c3682e9de9d53d9896368b81ab84
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-09-03 11:53:09 -0700

    release 2.4.3-SNAPSHOT-16

--- src/main/java/io/novaordis/events/metric/jboss/JBossCliMetricSource.java
+++ src/main/java/io/novaordis/events/metric/jboss/JBossCliMetricSource.java
@@ -221,7 +221,7 @@ public class JBossCliMetricSource implements MetricSource {
     @Override
     public String toString() {
 
-        return controllerAddress == null ? "null" : JBossCliMetricDefinition.PREFIX + controllerAddress.toString();
+        return controllerAddress == null ? "null" : controllerAddress.toString();
     }
 
     // Package protected -----------------------------------------------------------------------------------------------
